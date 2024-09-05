8360c82bcfb1acefa20791a90585c6af0d9c798c
gabriel guzman <gabrielguzman@gabriels-MacBook-Air.local>
2024-09-05 13:25:23 -0400

    refactor: remove unused function

--- frontendReact/src/components/structure/post/list/getListPosts.js
+++ frontendReact/src/components/structure/post/list/getListPosts.js
@@ -6,7 +6,7 @@ function PostList({ posts }) {
     <ul className="mdc-list">
       {posts.map(post => (
         <li key={post.id} className="mdc-list-item" tabIndex="0">
-          <Link to={`/post/${post.id}`} onClick={() => console.log('ok')}>
+          <Link to={`/post/${post.id}`}>
             <span className="mdc-list-item__text">{post.id}</span><br />
             <span className="mdc-list-item__text">{post.name}</span><br />
             <span className="mdc-list-item__text">{post.description}</span>
